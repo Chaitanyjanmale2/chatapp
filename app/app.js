af82fd2a3ea5415d641467ab15723d4f7d2c8975
Chaitany janmale <chaitanya.j@thinkbridge.in>
2023-07-19 14:06:24 +0530

    URLjjjhjjjhdcajdd

--- app/app.js
+++ app/app.js
@@ -1,30 +1,16 @@
-
-const socket = io('ws://localhost:1234');
-
+const socket = io('wss://mirage-pastoral-rowboat.glitch.me/', {
+    headers: {
+        'user-agent': 'Chrome'
+    }
+});
 socket.on('message', text => {
-
     const el = document.createElement('li');
     el.innerHTML = text;
-    document.querySelector('ul').appendChild(el)
-
+    document.querySelector('ul').appendChild(el);
 });
 
 document.querySelector('button').onclick = () => {
 
     const text = document.querySelector('input').value;
-    socket.emit('message', text)
-    
-}
-
-// Regular Websockets
-
-// const socket = new WebSocket('ws://localhost:8080');
-
-// // Listen for messages
-// socket.onmessage = ({ data }) => {
-//     console.log('Message from server ', data);
-// };
-
-// document.querySelector('button').onclick = () => {
-//     socket.send('hello');
-// }
+    socket.emit('message', text);
+};
